6bcb8aa9227db7d6d930fb8da6f9108d779af056
Gerard KESSE <tiakagerard@hotmail.com>
2020-08-17 08:38:12 +0200

    Initial Commit

--- code/GProject/src/gz_dart/bin/gz_dart.dart
+++
@@ -1,5 +0,0 @@
-import 'package:gz_dart/gz_dart.dart' as gz_dart;
-
-void main(List<String> arguments) {
-  print('Hello world: ${gz_dart.calculate()}!');
-}
--- code/GProject/src/gz_dart/test/gz_dart_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:gz_dart/gz_dart.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
